e943fc02c0254c5880295a09fbbba510f229ccb0
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2022-07-11 16:53:20 +0530

    Completed JS file of Home Page
    Completed JavaScript File of Home Page

--- js/main.js
+++ js/main.js
@@ -26,3 +26,24 @@ faqs.forEach(faq =>{
     })
 }
 )
+
+// Mobile Responsiveness for Tablets-Show/Hide navigation menu
+
+const menu=document.querySelector(".nav__menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+
+//Close navigation menu
+const closeNav = ()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+}
+
+closeBtn.addEventListener('click',closeNav)
